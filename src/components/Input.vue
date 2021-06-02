e4d2605735e2acc211f5fec730e0fc965362cdf2
Dustin Tran <dustin.andy.tran@gmail.com>
2021-06-03 02:24:26 +0900

    feat: create input component

---
+++ src/components/Input.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Add Task</h1>
+    <input v-model="taskTitle">
+    <button v-on:click="$emit('addTask', taskTitle)">Submit</button>
+</template>
+
+<script>
+
+export default {
+    data: () => ({
+        taskTitle: null,
+    }),
+    emits:{
+        addTask: String,
+    }
+}
+</script>
